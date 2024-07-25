8dbd2433ecf27a2fedc9a81f2c1a0a7af37e7b32
dalew75 <dalew75@gmail.com>
2024-07-25 12:28:21 -0400

    latest

--- config.py
+++ config.py
@@ -5,3 +5,5 @@ API_HASH = config('API_HASH')
 FORWARD = config('FORWARD', cast=bool, default=True)
 RELAY_MAP = config('RELAY_MAP')
 SESSION_NAME = config('SESSION_NAME', default='session')
+NATS_SERVER = config('NATS_SERVER');
+NATS_SUBJECT = config('NATS_SUBJECT', default='telegram-messages')
--- relay.py
+++ relay.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 import asyncio
 import logging
+import json
+from pprint import pformat
+import re
 
 from telethon import TelegramClient, events
-
+import nats
 import config
 
 logging.basicConfig(level=logging.INFO)
@@ -13,9 +16,13 @@ client = TelegramClient(config.SESSION_NAME, config.API_ID, config.API_HASH)
 client.start()
 
 RELAY_MAP = {}
+nats_client = None
 
 
 async def setup():
+    global nats_client
+    nats_client = await nats.connect(config.NATS_SERVER)
+
     user = await client.get_me()
     logger.info('Started serving as {}'.format(user.first_name))
     await client.get_dialogs()
@@ -26,19 +33,62 @@ async def setup():
 
         key, values = x.split(':', 1)
         values = values.split(',')
-        RELAY_MAP[int(key)] = [int(x) for x in values]
+        if key == 'default':
+            RELAY_MAP[key] = [int(x) for x in values]
+        else:
+            RELAY_MAP[int(key)] = [int(x) for x in values]
 
+
+def get_message_link(event):
+    if event.is_channel:
+        if event.chat.username:
+            return f"https://t.me/{event.chat.username}/{event.message.id}"
+        else:
+            return f"https://t.me/c/{event.chat.id}/{event.message.id}"
+    else:
+        return None
+    
+def extract_ca(text):
+    # Regex pattern to match the alphanumeric string after "CA:"
+    pattern = r"CA:\s*([A-Za-z0-9]+)"
+    
+    # Search for the pattern in the given text
+    match = re.search(pattern, text)
+    
+    # If a match is found, return the matched group (the alphanumeric string)
+    if match:
+        return match.group(1)
+    else:
+        return None
 
 @client.on(events.NewMessage)
 async def my_event_handler(event):
+    message_link = get_message_link(event)
     for chat_id, relays in RELAY_MAP.items():
         if event.chat and event.chat.id == chat_id:
+            
             for relay in relays:
+                logger.info('----------------------------------------------------')
                 logger.info('Sending message from {} to {}'.format(event.chat.id, relay))
-                if config.FORWARD:
-                    await client.forward_messages(relay, event.message)
-                else:
-                    await client.send_message(relay, event.message)
+                contractAddress = extract_ca(event.message.message)
+                is_tradeable = bool(re.search(r"pump\.fun", event.message.message, re.IGNORECASE))
+                
+                subject = config.NATS_SUBJECT
+                message = {
+                    "chat_id": event.chat.id,
+                    "contractAddress": contractAddress,
+                    "isTradeable": is_tradeable,
+                    "message": event.message.message,
+                    "message_link": message_link
+                }
+                await nats_client.publish(subject, json.dumps(message).encode())
+                
+                logger.info(contractAddress)
+                logger.info(event.message.message)
+                # if config.FORWARD:
+                #     await client.forward_messages(relay, event.message)
+                # else:
+                #     await client.send_message(relay, event.message)
             break
     else:
         for relay in RELAY_MAP.get('default', []):
@@ -46,7 +96,10 @@ async def my_event_handler(event):
             if config.FORWARD:
                 await client.forward_messages(relay, event.message)
             else:
-                await client.send_message(relay, event.message)
+                message_text = event.message.message
+                if message_link:
+                    message_text += f"\n\n[Original Message]({message_link})"
+                await client.send_message(relay, message_text, link_preview=False)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(setup())
